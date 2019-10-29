a271cb6fa53208b85f900233ba06dec7ab52a21e
Casey K <caseykelleher@icloud.com>
2019-10-28 21:18:50 -0400

    got all images to click and alert

---
+++ javascript/game.js
@@ -0,0 +1,21 @@
+
+$(".crystal-image-1").on("click", function() {
+
+    alert("You clicked the first jewel on the page");
+});
+
+$(".crystal-image-2").on("click", function() {
+
+    alert("You clicked the second jewel on the page");
+});
+
+$(".crystal-image-3").on("click", function() {
+
+    alert("You clicked the third jewel on the page");
+});
+
+$(".crystal-image-4").on("click", function() {
+
+    alert("You clicked the fourth jewel on the page");
+});
+
